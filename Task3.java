fe8abe9129e55d1200ab920c30d107194fe49766
maxespejojr <maximoespejojr@gmail.com>
2025-10-29 09:59:57 +0000

    Task 3

--- Task3.java
+++ Task3.java
@@ -1,1 +1,26 @@
+public class Task3{
+	
+	   public static void main(String[] args) {
+	
+	    String a = new String("Wow");
+	    String b = a;
+	    String c = "Wow!";
+	    String d = "Wow!";
+	
+	    boolean b1 = a == b;             // true 
+        boolean b2 = d.equals(b + "!");  // true 
+	    boolean b3 = !c.equals(a);       // true 
+	
+	    if (b1 && b2 && b3) {
+	        System.out.println("Success");
+	    }
+	}
+	      
+	  }
+    
+
+
+
+
+
 
